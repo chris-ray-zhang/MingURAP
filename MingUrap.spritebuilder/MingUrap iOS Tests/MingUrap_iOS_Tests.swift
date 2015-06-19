4a395f40a9d6e5bfcd941950181f021bc4412b1e
chris-ray-zhang <chris.ray.zhang@gmail.com>
2015-06-19 19:07:33 -0400

    apple pie bug fix

---
+++ MingUrap.spritebuilder/MingUrap iOS Tests/MingUrap_iOS_Tests.swift
@@ -0,0 +1,43 @@
+//
+//  MingUrap_iOS_Tests.swift
+//  MingUrap iOS Tests
+//
+//  Created by Chris Zhang on 6/19/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+
+
+class MingUrap_iOS_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testBasicImplementation() {
+        
+        
+    }
+    
+}
