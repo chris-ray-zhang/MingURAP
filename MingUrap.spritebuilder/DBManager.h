34f7c5e2152cd4f882674ccc7a1a9aac92006fed
Michael Zhang <michaelzhang333@gmail.com>
2015-06-30 17:47:14 -0700

    begin database

---
+++ MingUrap.spritebuilder/DBManager.h
@@ -0,0 +1,17 @@
+//
+//  DBManager.h
+//  MingUrap
+//
+//  Created by Michael Zhang on 6/30/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+-(instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+
+@end
+
+
